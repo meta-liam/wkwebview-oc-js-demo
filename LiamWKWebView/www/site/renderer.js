f2c0614d8c768658a10862bb5d8c6d3500dff398
liam1902 <liam.jiang@makeblock.com>
2022-03-21 11:52:44 +0800

    chore: 完善细节。

--- LiamWKWebView/www/site/renderer.js
+++ LiamWKWebView/www/site/renderer.js
@@ -1,12 +1,28 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
 console.log("---------11111-----------");
-
-
+//本方法兼容安卓与iOS
+function linkCallMobile(handlerInterface,handlerMethod,parameters){
+    //handlerInterface由iOS addScriptMessageHandler与andorid addJavascriptInterface 代码注入而来。
+    var dic = {'handlerInterface':handlerInterface,'function':handlerMethod,'parameters': parameters};
+    
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
+        window.webkit.messageHandlers[handlerInterface].postMessage(dic);
+    }else{
+        //安卓传输不了js json对象
+        window[handlerInterface][handlerMethod](JSON.stringify(parameters));
+    }
+}
+function linkCallMobileNative(parameters){
+    linkCallMobile("LinkNative","jsonrpc",parameters);
+    //showLog(parameters);//mock
+}
+function linkNativeCallBack(v){
+    //alert("linkNativeCallBack:"+v);
+    showLog(v);
+}
+// test:
+function linkTestCall(){
+    linkCallMobileNative('{"jsonrpc":"2.0","method":"say","params":"liam","id":1,"service":"hello-world"}');
+}
 console.log("---------0000-----------");
 var ws ;
 var inputMsg = document.getElementById("msgInput");
@@ -20,60 +36,27 @@ function showLog(v){
     outMsg.innerHTML =v+ "<br>"+ outMsg.innerHTML ;
 }
 
-function loadWs(){
-    console.log("loadWs!")
-    ws = new WebSocket("ws://localhost:52384");
-
-    ws.onopen = function(evt) { 
-        console.log("Connection open ..."); 
-        //ws.send('{"jsonrpc":"2.0","method":"getNetList","params":[],"id":0,"service":"discovery"}');
-    };
-
-    ws.onmessage = function(evt) {
-        // console.log( "Received Message: " + evt.data);
-        // ws.close();
-        showLog(evt.data)
-    };
-
-    ws.onclose = function(evt) {
-        console.log("Connection closed.");
-    }; 
-}
-
-function getList(){
-    ws.send('{"jsonrpc":"2.0","method":"getNetList","params":[],"id":0,"service":"discovery"}');
-}
 function sayHello(){
-    ws.send('{"jsonrpc":"2.0","method":"say","params":"liam","id":1,"service":"hello-world"}');
+    linkCallMobileNative('{"jsonrpc":"2.0","method":"say","params":"liam","id":1,"service":"hello-world"}');
 }
 
 document.getElementById("ts01").addEventListener('click',function ()
 {
-    showLog("hello!");
+    showLog("hello!16");
 }); 
-
-document.getElementById("tsLoadWs").addEventListener('click',function ()
-{
-    loadWs();
-});
 
 document.getElementById("tsSayHello").addEventListener('click',function ()
 {
     sayHello();
 });
 
-document.getElementById("tsGetList").addEventListener('click',function ()
-{
-    getList();
-});
-
-document.getElementById("tsCloseWs").addEventListener('click',function ()
-{
-    ws.close();
-});
+function clearString(v){
+    let st = v.replace('\n', "");
+    return st;
+}
 
 document.getElementById("tsSend").addEventListener('click',function ()
 {
-   var v = inputMsg.value ;
-   ws.send(v);
+    var v = clearString(""+ inputMsg.value);
+    linkCallMobileNative(v);
 });
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 > Objective-C写的简单使用WKWebView显示html的例子。
 > 加入Oc与js交换例子。
+
+## 预览
+
+![alt preview](./preview.jpg)
